dbdaf31aca227e01228cfd78ac1755df3cbdf8f3
Pedro Arcos Méndez <developer@pedroam.com>
2025-10-18 00:11:21 -0600

    Modified metrics

---
+++ metrics_mod.py
@@ -0,0 +1,23 @@
+import numpy as np
+from batch_generator_mod import minibatch_generator
+from utils_mod import int_to_onehot
+
+def compute_mse_and_acc(nnet, X, y, num_labels=10, minibatch_size=100):
+    mse, correct_pred, num_examples = 0., 0, 0
+    minibatch_gen = minibatch_generator(X, y, minibatch_size)
+        
+    for i, (features, targets) in enumerate(minibatch_gen):
+
+        _, probas, _ = nnet.forward(features)  # Añadido _ para z_h
+        predicted_labels = np.argmax(probas, axis=1)
+        
+        onehot_targets = int_to_onehot(targets, num_labels=num_labels)
+        loss = np.mean((onehot_targets - probas)**2)
+        correct_pred += (predicted_labels == targets).sum()
+        
+        num_examples += targets.shape[0]
+        mse += loss
+
+    mse = mse/(i+1)
+    acc = correct_pred/num_examples
+    return mse, acc
